6834970bfd4454ab73b0ce9ccaaa077e940e27b0
Romain Delhommais <romain@delhommais.com>
2026-01-03 18:34:40 +0100

    feat(zsh): add keybinds

--- hosts/luminix/users/lumine/zsh.nix
+++ hosts/luminix/users/lumine/zsh.nix
@@ -28,6 +28,13 @@
       # Basic Zsh behaviors
       setopt autocd
       setopt correct
+
+      # Binds
+      bindkey '^[[1;5C' forward-word
+      bindkey '^[[1;5D' backward-word
+      bindkey '^H'      backward-kill-word
+      bindkey '^ '      autosuggest-accept
     '';
   };
 }
+
