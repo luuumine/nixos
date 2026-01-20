41c17041de1d5821861e1cbec036b36d286a6815
Romain Delhommais <romain@delhommais.com>
2026-01-20 16:32:46 +0100

    feat(nvim): initial nvim barebones config

---
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -0,0 +1,25 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+
+vim.opt.mouse = ""
+
