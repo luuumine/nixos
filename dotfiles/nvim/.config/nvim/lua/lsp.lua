f138bb7fc2d80d93b47c2d5b96477cb8d33c457f
Romain Delhommais <romain@delhommais.com>
2026-01-21 21:11:02 +0100

    fix(nvim): lua lsp config and correct typos

--- dotfiles/nvim/.config/nvim/lua/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/lsp.lua
@@ -2,21 +2,21 @@
 local has_pyright = vim.fn.executable("pyright") == 1
 if has_pyright then
   vim.lsp.config("pyright", {})
-  vim.lsp.enable("pyright", {})
+  vim.lsp.enable("pyright")
 end
 
 -- Rust
 local has_rust_analyzer = vim.fn.executable("rust-analyzer") == 1
 if has_rust_analyzer then
   vim.lsp.config("rust_analyzer", {})
-  vim.lsp.enable("rust_analyzer", {})
+  vim.lsp.enable("rust_analyzer")
 end
 
 -- Nix
 local has_nixd = vim.fn.executable("nixd") == 1
 if has_nixd then
   vim.lsp.config("nixd", {})
-  vim.lsp.enable("nixd", {})
+  vim.lsp.enable("nixd")
 end
 
 -- Lua
@@ -29,9 +29,11 @@ if has_lua_ls then
           globals = { "vim" },
         },
         workspace = {
-          library = vim.api.nvim_get_runtime_file("", true),
+          library = { vim.env.VIMRUNTIME },
+          scanExternalLibraries = false,
           checkThirdParty = false,
         },
+        telemetry = { enable = false },
       },
     },
   })
