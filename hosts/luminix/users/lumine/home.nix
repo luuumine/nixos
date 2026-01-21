be70a2dff826d2d108a6516914c7f5ff83042ee2
Romain Delhommais <romain@delhommais.com>
2026-01-21 17:34:52 +0100

    feat(pkgs): add nix lsp and formatter

--- hosts/luminix/users/lumine/home.nix
+++ hosts/luminix/users/lumine/home.nix
@@ -1,14 +1,19 @@
 { config, pkgs, ... }:
 
+let
+  modules = ../../../../modules;
+  dev = "${modules}/dev";
+in
 {
   home-manager.users.lumine = {
     imports = [ 
       ./packages.nix
       ./zsh.nix
       ./hyprpaper.nix
-      ../../../../modules/nvim.nix
-      ../../../../modules/dev/python.nix
-      ../../../../modules/dev/rust.nix
+      "${modules}/nvim.nix"
+      "${modules}/dev/python.nix"
+      "${modules}/dev/rust.nix"
+      "${modules}/dev/nix.nix"
     ];
 
     home.username = "lumine";
