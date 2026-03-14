35e43930ff1ac9d0226bfddcdbbd9a225815cb6d
Romain Delhommais <romain@delhommais.com>
2026-03-14 18:42:57 +0100

    feat(nvim): add devicons and highlight colors plugins

--- dotfiles/nvim/.config/nvim/lua/plugins/qol.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/qol.lua
@@ -2,4 +2,6 @@ return {
 	{ "lewis6991/gitsigns.nvim", config = true },
 	{ "mbbill/undotree" },
 	{ "nvim-lualine/lualine.nvim", config = true },
+	{ "nvim-tree/nvim-web-devicons", config = true },
+	{ "brenoprata10/nvim-highlight-colors", config = true },
 }
