86a4367d6ca2a82691784a82b05f3524e0700824
Romain Delhommais <romain@delhommais.com>
2026-04-13 18:08:43 +0200

    fix(nvim): enable all lsps by default

--- dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,27 +2,25 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
-			local function enable_if(cmd, server, cfg)
-				if vim.fn.executable(cmd) == 1 then
+			local function enable(server, cfg)
 					vim.lsp.config(server, cfg or {})
 					vim.lsp.enable(server)
-				end
 			end
 
 			-- Python
-			enable_if("pyright", "pyright")
+			enable("pyright")
 
 			-- Rust
-			enable_if("rust-analyzer", "rust_analyzer")
+			enable("rust_analyzer")
 
 			-- Nix
-			enable_if("nixd", "nixd")
+			enable("nixd")
 
 			-- C/C++
-			enable_if("clangd", "clangd")
+			enable("clangd")
 
 			-- Lua
-			enable_if("lua-language-server", "lua_ls", {
+			enable("lua_ls", {
 				settings = {
 					Lua = {
 						diagnostics = {
@@ -39,7 +37,7 @@ return {
 			})
 
 			-- Typescript
-			enable_if("typescript-language-server", "ts_ls", {
+			enable("ts_ls", {
 				on_attach = function(client)
 					client.server_capabilities.documentFormattingProvider = false
 				end,
@@ -48,20 +46,20 @@ return {
 			-- Astro
 			-- Use the local project typescript library
 			local project_ts_path = vim.fn.getcwd() .. "/node_modules/typescript/lib"
-			enable_if("astro-ls", "astro", {
+			enable("astro", {
 				init_options = {
 					typescript = { tsdk = project_ts_path },
 				},
 			})
 
 			-- CSS
-			enable_if("vscode-css-language-server", "cssls")
+			enable("cssls")
 
 			-- JSON
-			enable_if("vscode-json-language-server", "jsonls")
+			enable("jsonls")
 
 			-- QML
-			enable_if("qmlls", "qmlls", {
+			enable("qmlls", {
 				cmd = { "qmlls", "-E" },
 				filetypes = { "qml", "qmljs" },
 				single_file_support = true,
