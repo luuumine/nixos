74e466129ccddb73620c2a9ee286b6b8341f4474
Romain Delhommais <romain@delhommais.com>
2026-04-23 20:36:13 +0200

    feat(secrets): add secrets dir, with cloudflare.age key

---
+++ secrets/secrets.nix
@@ -0,0 +1,19 @@
+let
+  # users
+  lumine = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOSF50b9uHqWXQgWC7T5dg2VMBYqI4T4I6VnEkm2R5aX lumine";
+
+  # systems
+  luminadel = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIgexWnRfrJ3PywZlt+h9ly0jclK+s6MThdKGt0g2JFY root@luminadel";
+  luminix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC9lU2TMuT7YUamnb9dNQsA00tVSd6gOAu721USnRBeP root@luminix";
+
+  # groups
+  users = [ lumine ];
+  systems = [
+    luminadel
+    luminix
+  ];
+  allKeys = users ++ systems;
+in
+{
+  "luminadel/cloudflare.age".publicKeys = users ++ [ luminadel ];
+}
