bda8196fa504078a6fc180193195a82cb9f7f59d
Romain Delhommais <romain@delhommais.com>
2026-08-30 18:20:15 +0200

    luminode is working

--- hosts/luminode/default.nix
+++ hosts/luminode/default.nix
@@ -1,10 +1,24 @@
 { self, inputs, ... }: {
   flake.nixosConfigurations.luminode = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = {
+      inherit inputs;
+      secretsPath = ../../secrets;
+    };
     modules = with self.nixosModules; [
       luminode-hardware
       luminode-filesystems
       luminode-config
       luminode-backups
+
+      ../../modules
+
+      inputs.home-manager.nixosModules.home-manager
+      {
+        home-manager.useGlobalPkgs = true;
+        home-manager.useUserPackages = true;
+        home-manager.extraSpecialArgs = { inherit inputs; };
+      }
+      inputs.agenix.nixosModules.default
     ];
   };
 }
