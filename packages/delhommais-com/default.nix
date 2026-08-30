c08f225cecfcdab6d767cc9e5a57ffb024821f07
Romain Delhommais <romain@delhommais.com>
2026-08-30 19:27:24 +0200

    delhommais-com

--- packages/delhommais-com/default.nix
+++ packages/delhommais-com/default.nix
@@ -1,48 +1,42 @@
 {
-  stdenv,
-  fetchPnpmDeps,
-  nodejs_22,
-  pnpmConfigHook,
-  pnpm_10,
-  astro-language-server,
-  typescript,
-  typescript-language-server,
-  vscode-langservers-extracted,
-  ...
-}:
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.delhommais-com =
+        let
+          pnpm = pkgs.pnpm_10;
+          pname = "delhommais-com";
+          version = "4.0.1";
+          src = ./.;
+        in
+        pkgs.stdenv.mkDerivation {
+          inherit pname version src;
 
-let
-  pnpm = pnpm_10;
-  pname = "delhommais-com";
-  version = "4.0.1";
-  src = ./.;
-in
-stdenv.mkDerivation {
-  inherit pname version src;
+          nativeBuildInputs = [
+            pkgs.nodejs_22
+            pkgs.pnpmConfigHook
+            pnpm
+            pkgs.astro-language-server
+            pkgs.typescript
+            pkgs.typescript-language-server
+            pkgs.vscode-langservers-extracted
+          ];
 
-  nativeBuildInputs = [
-    nodejs_22
-    pnpmConfigHook
-    pnpm
-    astro-language-server
-    typescript
-    typescript-language-server
-    vscode-langservers-extracted
-  ];
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit pname version src;
+            inherit pnpm;
+            fetcherVersion = 3;
+            hash = "sha256-hUw5Emvimzhs1Wz1ZvPLw85aowAKCKKZx50PKsFUMIg=";
+          };
 
-  pnpmDeps = fetchPnpmDeps {
-    inherit pname version src;
-    inherit pnpm;
-    fetcherVersion = 3;
-    hash = "sha256-hUw5Emvimzhs1Wz1ZvPLw85aowAKCKKZx50PKsFUMIg=";
-  };
+          buildPhase = "pnpm build";
+          installPhase = "cp -r dist $out";
 
-  buildPhase = "pnpm build";
-  installPhase = "cp -r dist $out";
-
-  shellHook = ''
-    export PATH="$PWD/node_modules/.bin:$PATH"
-    export NODE_PATH="${typescript}/lib/node_modules:$NODE_PATH"
-    echo "astro development environment active"
-  '';
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            export NODE_PATH="${pkgs.typescript}/lib/node_modules:$NODE_PATH"
+            echo "astro development environment active"
+          '';
+        };
+    };
 }
