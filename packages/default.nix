6c4370be72dc584b9b2689efd1698d6617b3b61b
Romain Delhommais <romain@delhommais.com>
2026-08-30 19:27:53 +0200

    packages

--- packages/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs }:
-
-{
-  api-lumine = pkgs.callPackage ./api-lumine { };
-  delhommais-com = pkgs.callPackage ./delhommais-com { };
-  killer-game = pkgs.callPackage ./killer-game { };
-  luuumine-com = pkgs.callPackage ./luuumine-com { };
-  quickshell-lumine = pkgs.callPackage ./quickshell-lumine { };
-}
