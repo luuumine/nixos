a74c2972ed560673806cbcc058e5ce5d207e3a40
Romain Delhommais <romain@delhommais.com>
2026-08-30 19:23:03 +0200

    killer-game

--- packages/killer-game/default.nix
+++ packages/killer-game/default.nix
@@ -1,53 +1,49 @@
 {
-  stdenv,
-  fetchPnpmDeps,
-  nodejs_22,
-  pnpmConfigHook,
-  pnpm_10,
-  astro-language-server,
-  typescript,
-  typescript-language-server,
-  vscode-langservers-extracted,
-}:
-let
-  pnpm = pnpm_10.override { nodejs-slim = nodejs_22; };
-  pname = "killer-game";
-  version = "1.1.1";
-  src = ./.;
-in
-stdenv.mkDerivation {
-  inherit pname version src;
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.killer-game =
+        let
+          pnpm = pkgs.pnpm_10.override { nodejs-slim = pkgs.nodejs_22; };
+          pname = "killer-game";
+          version = "1.1.1";
+          src = ./.;
+        in
+        pkgs.stdenv.mkDerivation {
+          inherit pname version src;
 
-  nativeBuildInputs = [
-    nodejs_22
-    pnpmConfigHook
-    pnpm
-    astro-language-server
-    typescript
-    typescript-language-server
-    vscode-langservers-extracted
-  ];
+          nativeBuildInputs = [
+            pkgs.nodejs_22
+            pkgs.pnpmConfigHook
+            pnpm
+            pkgs.astro-language-server
+            pkgs.typescript
+            pkgs.typescript-language-server
+            pkgs.vscode-langservers-extracted
+          ];
 
-  pnpmDeps = fetchPnpmDeps {
-    inherit pname version src;
-    inherit pnpm;
-    fetcherVersion = 3;
-    hash = "sha256-Ops0pA+yhpY/eTYNZT/JJiEGBCpEQkmWvRzCm+4vuU4=";
-  };
+          pnpmDeps = pkgs.fetchPnpmDeps {
+            inherit pname version src;
+            inherit pnpm;
+            fetcherVersion = 3;
+            hash = "sha256-Ops0pA+yhpY/eTYNZT/JJiEGBCpEQkmWvRzCm+4vuU4=";
+          };
 
-  buildPhase = ''
-    pnpm build
-  '';
-  installPhase = ''
-    mkdir -p $out
-    cp -r dist $out/
-    cp -r node_modules $out/
-    cp package.json $out/
-  '';
+          buildPhase = ''
+            pnpm build
+          '';
+          installPhase = ''
+            mkdir -p $out
+            cp -r dist $out/
+            cp -r node_modules $out/
+            cp package.json $out/
+          '';
 
-  shellHook = ''
-    export PATH="$PWD/node_modules/.bin:$PATH"
-    export NODE_PATH="${typescript}/lib/node_modules:$NODE_PATH"
-    echo "killer development environment active"
-  '';
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            export NODE_PATH="${pkgs.typescript}/lib/node_modules:$NODE_PATH"
+            echo "killer development environment active"
+          '';
+        };
+    };
 }
