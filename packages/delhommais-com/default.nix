143768b110d1da56e8cc2021a0675e953f084a91
Romain Delhommais <romain@delhommais.com>
2026-09-07 01:00:23 +0200

    chore(delhommais-com): ditch astro for html/css

--- packages/delhommais-com/default.nix
+++ packages/delhommais-com/default.nix
@@ -1,48 +1,25 @@
 {
   stdenv,
-  fetchPnpmDeps,
-  nodejs_22,
-  pnpmConfigHook,
-  pnpm_10,
-  astro-language-server,
-  typescript,
-  typescript-language-server,
   vscode-langservers-extracted,
+  prettier,
   ...
 }:
-
 let
-  pnpm = pnpm_10;
   pname = "delhommais-com";
-  version = "4.0.1";
-  src = ./.;
+  version = "4.1.0";
+  src = ./src;
 in
 stdenv.mkDerivation {
   inherit pname version src;
 
   nativeBuildInputs = [
-    nodejs_22
-    pnpmConfigHook
-    pnpm
-    astro-language-server
-    typescript
-    typescript-language-server
     vscode-langservers-extracted
+    prettier
   ];
 
-  pnpmDeps = fetchPnpmDeps {
-    inherit pname version src;
-    inherit pnpm;
-    fetcherVersion = 3;
-    hash = "sha256-hUw5Emvimzhs1Wz1ZvPLw85aowAKCKKZx50PKsFUMIg=";
-  };
-
-  buildPhase = "pnpm build";
-  installPhase = "cp -r dist $out";
-
-  shellHook = ''
-    export PATH="$PWD/node_modules/.bin:$PATH"
-    export NODE_PATH="${typescript}/lib/node_modules:$NODE_PATH"
-    echo "astro development environment active"
+  dontBuild = true;
+  installPhase = ''
+    mkdir -p $out
+    cp -r . $out/
   '';
 }
